1646714c6f7642f99eceb2dce6e6f0d6ba404fab
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2016-09-19 14:16:48 -0300

    Add files via upload
    Archivos .java

---
+++ Choose&Travel_java/Cuenta_Usuario.java
@@ -0,0 +1,46 @@
+
+public class Cuenta_Usuario
+{
+    private String user;
+    private String pass;
+    private boolean admin;
+
+    public Cuenta_Usuario()
+    {
+        user=null;
+        pass=null;
+        admin=false;
+    }
+
+    public void setUser(String u)
+    {
+        user=u;
+    }
+    
+    public String getUser()
+    {
+        return user;
+    }
+    
+    public void setPass(String p)
+    {
+        pass=p;
+    }
+    
+    public String getPass()
+    {
+        return pass;
+    }
+    
+    public void setAdmin(boolean a)
+    {
+        admin=a;
+    }
+    
+    public boolean getAdmin()
+    {
+        return admin;
+    }
+    
+    
+}
